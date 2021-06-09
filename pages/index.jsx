958dd124369827f27ca44c21f951445b9db6662d
Onur Temiz <onurmain@gmail.com>
2021-06-09 22:54:42 +0300

    build: Home Container

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,13 +3,6 @@ import Link from 'next/link';
 import stylex from '@ladifire-opensource/stylex';
 
 const styles = stylex.create({
-  topLogo: {
-    height: '5em',
-    marginBottom: 16
-  },
-  logo: {
-    height: '1.5em'
-  },
   container: {
     minHeight: '100vh',
     paddingTop: 0,
@@ -29,50 +22,37 @@ const styles = stylex.create({
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-    justifyContent: 'center',
     alignItems: 'center'
   },
   title: {
-    margin: 0,
-    lineHeight: 1.15,
-    fontSize: '4rem',
-    textAlign: 'center'
+    fontSize: '5rem',
+    margin: 15
   },
-  description: {
-    textAlign: 'center',
-    lineHeight: 1.5,
-    fontSize: '1.5rem'
-  },
-  code: {},
-  grid: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    maxWidth: 800,
-    marginTop: '3rem',
+  wrapper: {},
 
-    // @ts-ignore
-    '@media (max-width: 600px)': {
-      width: '100%',
-      flexDirection: 'column'
-    }
+  button: {
+    color: '#fff',
+    backgroundColor: '#1D4ED8',
+    paddingTop: '0.75rem',
+    paddingBottom: '0.75rem',
+    paddingLeft: '1.5rem',
+    paddingRight: '1.5rem',
+    border: 'none',
+    fontWeight: 'bold',
+    borderRadius: '0.25rem',
+    fontSize: '1.25rem',
+    marginLeft: 15,
+    cursor: 'pointer'
   },
-  card: {
-    margin: '1rem',
-    flexBasis: '45%',
-    padding: '1.5rem',
-    textAlign: 'left',
-    border: '1px solid #eaeaea',
-    borderRadius: 10,
-    textDecoration: 'none',
-    transition: 'color 0.15s ease, border-color 0.15s ease',
 
-    ':hover': {
-      color: '#0070f3',
-      borderColor: '#0070f3'
-    }
+  input: {
+    width: 300,
+    paddingTop: '0.75rem',
+    paddingBottom: '0.75rem',
+    paddingLeft: '0.5rem',
+    paddingRight: '0.5rem'
   },
+
   footer: {
     width: '100%',
     height: 100,
@@ -91,56 +71,24 @@ export default function Home() {
   return (
     <div className={stylex(styles.container)}>
       <Head>
-        <title>Stylex - Write CSS in JS with atomic, like Facebook do!</title>
+        <title>Repo Rankr</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={stylex(styles.main)}>
-        <img
-          src="/logo-black.png"
-          alt="Vercel Logo"
-          className={stylex(styles.topLogo)}
-        />
-        <h1 className={stylex(styles.title)}>Write CSS-in-JS with atomic!</h1>
-
-        <p className={stylex(styles.description)}>like Facebook do.</p>
-
-        <div className={stylex(styles.grid)}>
-          <Link href="/setup">
-            <a className={stylex(styles.card)}>
-              <h3>Setup &rarr;</h3>
-              <p>Learn how to setup Stylex for your project.</p>
-            </a>
-          </Link>
-          <Link href="/docs">
-            <a className={stylex(styles.card)}>
-              <h3>Documentation &rarr;</h3>
-              <p>Find in-depth information about Stylex features.</p>
-            </a>
-          </Link>
-          <Link href="/showcases">
-            <a className={stylex(styles.card)}>
-              <h3>Showcases &rarr;</h3>
-              <p>Something that&apos;s used with Stylex.</p>
-            </a>
-          </Link>
-          <Link href="/community">
-            <a className={stylex(styles.card)}>
-              <h3>Community &rarr;</h3>
-              <p>Joint Stylex community.</p>
-            </a>
-          </Link>
+        <h1 className={stylex(styles.title)}>Repo Rankr</h1>
+        <div className={stylex(styles.wrapper)}>
+          <input
+            className={stylex(styles.input)}
+            placeholder="https://github.com/ladifire-opensource/stylex"
+          />
+          <button className={stylex(styles.button)}>Analyze</button>
         </div>
       </main>
 
       <footer className={stylex(styles.footer)}>
         <span className={stylex(styles.footerContent)}>
-          This site&apos;s built with{' '}
-          <img
-            src="/logo-black.png"
-            alt="Stylex Logo"
-            className={stylex(styles.logo)}
-          />
+          This site is built with ❤️
         </span>
       </footer>
     </div>
