c0cc0ff48308d82b7c6a37eb91366ff1036babb6
Navinn Ravindaran <navinn.ravindaran@mail.utoronto.ca>
2021-06-10 08:35:42 -0400

    feat: add badge and json support for api call

--- pages/api/[owner]/[name].js
+++ pages/api/[owner]/[name].js
@@ -3,13 +3,17 @@ import getRepository from '../../../lib/getRepository';
 import generateSvg from '../../../lib/generateSvg';
 
 export default async (req, res) => {
-  const { owner, name } = req.query;
+  const { owner, name, badge } = req.query;
   const data = await getRepository(owner, name);
 
   const score = req.query.score ?? '100';
   const svg = generateSvg(score);
 
-  // res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
-  // res.write(svg);
-  res.status(200).json(data);
+  if (!!badge && badge === 'true') {
+    res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
+    res.write(svg);
+    res.end();
+  } else {
+    res.status(200).json(data);
+  }
 };
